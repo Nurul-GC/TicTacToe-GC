406934e3e803957e3dbd60f9c33f71c3bf531eb8
Nurul-GC <Nurul-GC@github.com>
2021-01-13 14:01:36 -0100

    updating commit

---
+++ github.sh
@@ -0,0 +1,8 @@
+git init
+git add ./source.java/*
+git add ./source.py/*
+git add ./*
+git commit -m "updating commit"
+git remote add origin https://github.com/Nurul-GC/TicTacToe-GC.git
+git branch -M main
+git push -u origin main
---
+++ source.java/tictactoe_gc/src/tictactoe_cli.java
@@ -0,0 +1,13 @@
+/************************************************
+ * Direitos Autorais (c) 2019-2021. Nurul GC    *
+ *                                              *
+ * Jovem Programador                            *
+ * Estudante de Engenharia de Telecomunicações  *
+ * Tecnologia de Informação e de Medicina       *
+ * Fé Foco Força Paciência                      *
+ * Allah no Comando.                            *
+ ************************************************/
+
+public class tictactoe_cli {
+    //
+}
