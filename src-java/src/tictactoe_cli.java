cb8d9563a6852c6b552e2d9f13d5f1e8c472e34a
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-19 16:12:44 +0200

    update src-java

--- src-java/src/tictactoe_cli.java
+++ src-java/src/tictactoe_cli.java
@@ -14,7 +14,6 @@ public class tictactoe_cli {
     public static void main(String[] args){
         char peca = 'X';
         int rodada = 0;
-        char espacos = ' ';
         String[] quadro = {
                 String.valueOf(' '), String.valueOf(' '), String.valueOf(' '),
                 String.valueOf(' '), String.valueOf(' '), String.valueOf(' '),
